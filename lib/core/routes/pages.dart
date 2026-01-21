2d6d27754ddca04d132ecd1b347d33419cf440b4
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-21 20:32:58 +0700

    feat: add routing structure with Pages and Routes classes for navigation

---
+++ lib/core/routes/pages.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+import 'package:sukientotapp/core/utils/import/screens.dart';
+import 'package:sukientotapp/core/utils/import/binding.dart';
+
+part 'routes.dart';
+
+class Pages {
+  Pages._();
+
+  static const initialRoute = Routes.splashScreen;
+
+  static final routes = [
+    GetPage(
+      name: Routes.splashScreen,
+      page: () => const SplashScreen(),
+      binding: SplashBinding(),
+    ),
+  ];
+}
---
+++ lib/core/routes/routes.dart
@@ -0,0 +1,7 @@
+part of 'pages.dart';
+
+abstract class Routes {
+  Routes._();
+
+  static const splashScreen = '/splash';
+}
