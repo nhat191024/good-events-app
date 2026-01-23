d9e6b46b2f51d19644d9ef041df751cb2ffc1c6a
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-23 09:22:17 +0700

    feat: add choose_yo_side_binding export to binding utility

--- lib/core/utils/import/binding.dart
+++ lib/core/utils/import/binding.dart
@@ -1,1 +1,4 @@
+// Add other binding exports here as needed
+
 export 'package:sukientotapp/core/bindings/splash_binding.dart';
+export 'package:sukientotapp/core/bindings/choose_yo_side_binding.dart';
