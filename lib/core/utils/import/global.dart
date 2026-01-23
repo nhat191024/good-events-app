b9db06076891bd3316599859f342fd18a72041a9
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-23 09:22:29 +0700

    feat: add global imports for core utilities and commonly used packages

---
+++ lib/core/utils/import/global.dart
@@ -0,0 +1,10 @@
+// Global imports for the project
+export 'package:get/get.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/foundation.dart';
+
+// Exporting core utilities
+export 'package:sukientotapp/core/utils/logger.dart';
+
+// Global exports for commonly used packages
+// Add other global exports if that package are frequently used across the app
