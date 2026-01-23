284d1cf0e29a3ba672e2966ad3830948c59e9af6
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-23 09:58:36 +0700

    feat: add comment for future screen exports in screens.dart

--- lib/core/utils/import/screens.dart
+++ lib/core/utils/import/screens.dart
@@ -1,2 +1,4 @@
+// add other screen exports here as needed
+
 export 'package:sukientotapp/features/common/splash/splash_screen.dart';
 export 'package:sukientotapp/features/common/choose_yo_side/choose_yo_side_screen.dart';
