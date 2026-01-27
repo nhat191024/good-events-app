66c445f4047de02a1ce00c5527c9b01cace0c76e
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-27 20:18:59 +0700

    feat: update SplashBinding to include lazy loading for dependencies

--- lib/core/bindings/splash_binding.dart
+++ lib/core/bindings/splash_binding.dart
@@ -1,9 +1,16 @@
 import 'package:get/get.dart';
 import 'package:sukientotapp/features/common/splash/splash_controller.dart';
+import 'package:sukientotapp/data/repositories/auth_repository_impl.dart';
+import 'package:sukientotapp/data/providers/auth_provider.dart';
+import 'package:sukientotapp/domain/repositories/auth_repository.dart';
+import 'package:sukientotapp/core/services/api_service.dart';
 
 class SplashBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put<SplashController>(SplashController());
+    Get.lazyPut<ApiService>(() => ApiService(), fenix: true);
+    Get.lazyPut<AuthProvider>(() => AuthProvider(Get.find<ApiService>()));
+    Get.lazyPut<AuthRepository>(() => AuthRepositoryImpl(Get.find<AuthProvider>()));
+    Get.put<SplashController>(SplashController(Get.find<AuthRepository>()));
   }
 }
