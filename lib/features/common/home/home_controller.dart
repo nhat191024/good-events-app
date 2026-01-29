1a83dbf8f2efa31c009e6a9af52d0c0a8fda8a7a
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-29 16:36:26 +0700

    feat: update userType logic in GuestHomeController based on isServiceProvider

--- lib/features/common/home/home_controller.dart
+++ lib/features/common/home/home_controller.dart
@@ -1,5 +1,12 @@
 import 'package:sukientotapp/core/utils/import/global.dart';
 
 class GuestHomeController extends GetxController {
-  RxBool userType = true.obs; // true: customer, false: service provider
+  var isServiceProvider = Get.arguments['isServiceProvider'] as bool? ?? false;
+  RxBool userType = false.obs; // true: customer, false: service provider
+
+  @override
+  void onInit() {
+    super.onInit();
+    userType.value = !isServiceProvider;
+  }
 }
