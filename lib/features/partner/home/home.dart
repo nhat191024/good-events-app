dd9708a35e73704633950cf4087b74d6cd083834
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-02-01 02:42:24 +0700

    feat: update HomeScreen layout with user verification badge and improved structure

--- lib/features/partner/home/home.dart
+++ lib/features/partner/home/home.dart
@@ -4,6 +4,11 @@ import 'package:sukientotapp/features/partner/home/home_controller.dart';
 import 'package:sukientotapp/features/components/common/language_switch/language_switch.dart';
 import 'package:sukientotapp/features/components/common/notification_button/notification_button.dart';
 
+import 'widgets/qick_actions_panel.dart';
+import 'widgets/income_panel.dart';
+import 'widgets/bill_count_panel.dart';
+import 'widgets/new_review_panel.dart';
+
 class HomeScreen extends GetView<HomeController> {
   const HomeScreen({super.key});
 
@@ -18,26 +23,42 @@ class HomeScreen extends GetView<HomeController> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 FAvatar(
                   image: NetworkImage(controller.avatar.value),
-                  size: 40.0,
+                  size: 46.0,
                   semanticsLabel: 'User avatar',
                   fallback: const Text('ST'),
                 ),
                 const SizedBox(width: 10),
-                Text(
-                  controller.name.value,
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      controller.name.value,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    FBadge(child: Text('verified'.tr)),
+                  ],
                 ),
               ],
             ),
-            Row(children: [LanguageSwitch(), NotificationButton(isHaveNoti: true,)]),
+            Row(children: [LanguageSwitch(), const SizedBox(width: 10), const NotificationButton(isHaveNoti: true)]),
           ],
         ),
       ),
-      child: Center(
-        child: Text('partner_home'.tr),
+      child: Column(
+        children: [
+          const SizedBox(height: 16),
+          const IncomePanel(),
+          const SizedBox(height: 16),
+          const QickActionsPanel(),
+          const SizedBox(height: 16),
+          const BillCountPanel(),
+          const SizedBox(height: 16),
+          const NewReviewPanel(),
+        ],
       ),
     );
   }
