1b2717e50c26598d9be5559a058f61a19d6476fb
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-02-01 02:41:51 +0700

    feat: enhance Vietnamese and English translations for partner home and show sections

--- lib/core/utils/app_translations.dart
+++ lib/core/utils/app_translations.dart
@@ -26,16 +26,32 @@ class AppTranslations extends Translations {
       'partner_register_now': 'Đăng ký để bắt đầu làm việc ngay hôm nay!',
       'customer_register_now': 'Đăng ký để trải nghiệm dịch vụ ngay hôm nay!',
 
+      //partner home
+      'quick_actions': 'Truy cập nhanh',
+      'view_details': 'Xem chi tiết',
+      'new_rate': '@rates Đánh giá mới',
+      'from_clients': 'từ khách hàng',
+
       //common
       'home': 'Trang chủ',
       'bills': 'Đơn',
-      'take_order': 'Nhận show',
       'messages': 'Tin nhắn',
       'account': 'Tài khoản',
+
+      'wallet': 'Ví',
+      'income': 'Thu nhập',
+
+      'calendar': 'Lịch',
 
       'customer': 'Khách hàng',
       'partner': 'Đối tác',
       'service_provider': 'Nhà cung cấp dịch vụ',
+
+      ///Show
+      'take_order': 'Nhận show',
+      'waiting': 'Đang chờ',
+      'confirmed': 'Đã xác nhận',
+      'new_show': 'Show mới',
 
       'notification': 'Thông báo',
       'login_successful': 'Đăng nhập thành công! Welcome, @name.',
@@ -76,16 +92,32 @@ class AppTranslations extends Translations {
       'partner_register_now': 'Register to start working today!',
       'customer_register_now': 'Register to experience the service today!',
 
+      //partner home
+      'quick_actions': 'Quick Actions',
+      'view_details': 'View Details',
+      'new_rate': '@rates New Reviews',
+      'from_clients': 'from customers',
+
       //common
       'home': 'Home',
       'bills': 'Bills',
-      'take_order': 'Take show',
       'messages': 'Messages',
       'account': 'Account',
-      
+
+      'wallet': 'Ví',
+      'income': 'Thu nhập',
+
+      'calendar': 'Calendar',
+
       'customer': 'Customer',
       'partner': 'Partner',
       'service_provider': 'Service Provider',
+
+      ///Show
+      'take_order': 'Take show',
+      'waiting': 'Waiting',
+      'confirmed': 'Confirmed',
+      'new_show': 'New Show',
 
       'notification': 'Notification',
       'login_successful': 'Login successful! Welcome, @name.',
