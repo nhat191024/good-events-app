7dc7f68a03e16da4d98c6fc50846e10695644fbc
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-02-06 13:30:07 +0700

    feat: add ContextX extension for enhanced BuildContext utilities

---
+++ lib/core/utils/context_extensions.dart
@@ -0,0 +1,22 @@
+import 'package:forui/forui.dart';
+import 'package:flutter/material.dart';
+
+extension ContextX on BuildContext {
+  FThemeData get fTheme => FTheme.of(this);
+  ThemeData get theme => Theme.of(this);
+
+  Size get mediaQuerySize => MediaQuery.sizeOf(this);
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+
+  // Truy cập nhanh Typography: context.typography
+  FTypography get typography => fTheme.typography;
+
+  //Get theme colors
+  Color get primary => theme.colorScheme.primary;
+
+  //Get screen dimensions
+  double get screenWidth => mediaQuerySize.width;
+  double get screenHeight => mediaQuerySize.height;
+
+  double get statusBarHeight => MediaQuery.of(this).viewPadding.top;
+}
