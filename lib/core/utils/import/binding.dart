2594bdc999739833a6949721be5da6c886748d3c
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-02-06 09:55:36 +0700

    refactor: change binding file name

--- lib/core/utils/import/binding.dart
+++ lib/core/utils/import/binding.dart
@@ -1,11 +1,11 @@
 // Add other binding exports here as needed
 
-export 'package:sukientotapp/features/common/splash/splash_binding.dart';
-export 'package:sukientotapp/features/common/choose_yo_side/choose_yo_side_binding.dart';
-export 'package:sukientotapp/features/common/auth/login/login_binding.dart';
+export 'package:sukientotapp/features/common/splash/binding.dart';
+export 'package:sukientotapp/features/common/choose_yo_side/binding.dart';
+export 'package:sukientotapp/features/common/auth/login/binding.dart';
 
 //Guest
-export 'package:sukientotapp/features/common/home/guest_home_binding.dart';
+export 'package:sukientotapp/features/common/home/binding.dart';
 
 //Partner
-export 'package:sukientotapp/features/partner/bottom_navigation/bottom_navigation_binding.dart';
+export 'package:sukientotapp/features/partner/bottom_navigation/binding.dart';
