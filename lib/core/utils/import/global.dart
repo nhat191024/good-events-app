e5cd8e0d2d83642344cd3942d19b4a5b40430197
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-02-06 13:30:14 +0700

    refactor: reorganize exports in global.dart for better clarity

--- lib/core/utils/import/global.dart
+++ lib/core/utils/import/global.dart
@@ -12,6 +12,9 @@ export 'package:sukientotapp/core/utils/app_images.dart';
 export 'package:sukientotapp/core/utils/app_colors.dart';
 export 'package:sukientotapp/core/services/localstorage_service.dart';
 
+// Exporting context extensions
+export 'package:sukientotapp/core/utils/context_extensions.dart';
+
 // Icon packs
 export 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
