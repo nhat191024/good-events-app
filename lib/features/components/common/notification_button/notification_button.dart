f7eaa4978ab8be937ba854ffee3358d8d4a01215
Peredar <64675226+perad4r@users.noreply.github.com>
2026-02-08 06:28:29 +0700

    update: add ringing effect when hasNotification==true and fun clicking effect

--- lib/features/components/common/notification_button/notification_button.dart
+++ lib/features/components/common/notification_button/notification_button.dart
@@ -1,31 +1,55 @@
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:sukientotapp/core/utils/import/global.dart';
 
-class NotificationButton extends StatelessWidget {
+class NotificationButton extends StatefulWidget {
   final bool hasNotification;
-  const NotificationButton({super.key, this.hasNotification = false});
+  final VoidCallback? onTap;
+  const NotificationButton({super.key, this.hasNotification = false, this.onTap});
+
+  @override
+  State<NotificationButton> createState() => _NotificationButtonState();
+}
+
+class _NotificationButtonState extends State<NotificationButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
-      child: Container(
-        decoration: const BoxDecoration(color: Colors.white, shape: BoxShape.circle),
-        padding: const EdgeInsets.all(12),
-        width: 50,
-        height: 60,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Icon(FIcons.bell, color: FTheme.of(context).colors.primary),
-            if (hasNotification)
-              Positioned(
-                top: 5,
-                right: 4,
-                child: CircleAvatar(backgroundColor: Colors.red[900], radius: 4),
-              ),
-          ],
-        ),
-      ),
+      onTap: () {
+        // trigger animation
+        _controller?.forward().then((_) => _controller?.reverse());
+        widget.onTap?.call();
+      },
+      child:
+          Container(
+                decoration: const BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+                padding: const EdgeInsets.all(12),
+                width: 50,
+                height: 60,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    widget.hasNotification
+                        ? Icon(FIcons.bell, color: FTheme.of(context).colors.primary)
+                              .animate(onPlay: (controller) => controller.repeat(reverse: true))
+                              .shake(rotation: 0.3, hz: 2)
+                        : Icon(FIcons.bell, color: FTheme.of(context).colors.primary),
+                    if (widget.hasNotification)
+                      Positioned(
+                        top: 5,
+                        right: 4,
+                        child: CircleAvatar(backgroundColor: Colors.red[900], radius: 4),
+                      ),
+                  ],
+                ),
+              )
+              .animate(
+                autoPlay: false,
+                onInit: (controller) => _controller = controller,
+              )
+              .scaleXY(end: 0.85, duration: 100.ms, curve: Curves.easeInOut),
     );
   }
 }
