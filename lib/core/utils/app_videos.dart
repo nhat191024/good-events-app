6efef9ca4949f3add99ff7b522335a8098815eb6
Peredar <64675226+perad4r@users.noreply.github.com>
2026-02-07 12:17:34 +0700

    update: import video class

---
+++ lib/core/utils/app_videos.dart
@@ -0,0 +1,4 @@
+class AppVideos {
+  static const String splashVideoLight = 'assets/videos/splash_video_light.mp4';
+  static const String splashVideoDark = 'assets/videos/splash_video_dark.mp4';
+}
