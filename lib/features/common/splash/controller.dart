a014b1bcfd095a3723a90d7a81b411d4e36e65f2
Peredar <64675226+perad4r@users.noreply.github.com>
2026-02-07 12:22:08 +0700

    add: video player and handlers

--- lib/features/common/splash/controller.dart
+++ lib/features/common/splash/controller.dart
@@ -1,11 +1,21 @@
+import 'dart:async';
+
 import 'package:sukientotapp/core/utils/import/global.dart';
+import 'package:sukientotapp/core/utils/app_videos.dart';
 import 'package:sukientotapp/domain/repositories/auth_repository.dart';
+import 'package:video_player/video_player.dart';
 
 //TODO: update splash in future for now just a placeholder
 class SplashController extends GetxController {
   final AuthRepository _authRepository;
 
   SplashController(this._authRepository);
+
+  late VideoPlayerController videoPlayerController;
+  RxBool isVideoInitialized = false.obs;
+  final Completer<void> _videoCompleter = Completer<void>();
+
+  final bool isDarkMode = Get.isDarkMode;
 
   @override
   void onInit() {
@@ -19,7 +29,35 @@ class SplashController extends GetxController {
       Get.updateLocale(Locale(code, country));
     }
 
+    _initVideo();
     _checkToken();
+  }
+
+  @override
+  void onClose() {
+    /// mem leak ;)
+    videoPlayerController.dispose();
+    super.onClose();
+  }
+
+  void _initVideo() async {
+    videoPlayerController = VideoPlayerController.asset(
+      isDarkMode ? AppVideos.splashVideoDark : AppVideos.splashVideoLight,
+    );
+    await videoPlayerController.initialize();
+    isVideoInitialized.value = true;
+    logger.i('preparing to play video');
+    videoPlayerController.play();
+
+    videoPlayerController.addListener(() {
+      logger.i('videoPlayerController is playing');
+      if (videoPlayerController.value.position >= videoPlayerController.value.duration) {
+        if (!_videoCompleter.isCompleted) {
+          logger.w('video completed');
+          _videoCompleter.complete();
+        }
+      }
+    });
   }
 
   ///Check token validity
@@ -30,6 +68,7 @@ class SplashController extends GetxController {
 
     if (token == null) {
       logger.w('[SplashController] [_checkToken] No token found, redirecting to choose side');
+      await _videoCompleter.future;
       Get.offAllNamed(Routes.chooseYoSideScreen);
       return;
     }
@@ -41,9 +80,11 @@ class SplashController extends GetxController {
         var role = StorageService.readMapData(key: LocalStorageKeys.user, mapKey: 'role');
         switch (role) {
           case 'client':
-            Get.snackbar('notification'.tr, 'in_dev'.tr);
+            await _videoCompleter.future;
+            Get.offAllNamed(Routes.clientHome);
             return;
           case 'partner':
+            await _videoCompleter.future;
             Get.offAllNamed(Routes.partnerHome);
             return;
         }
@@ -51,11 +92,13 @@ class SplashController extends GetxController {
         logger.w('[SplashController] [_checkToken] Token invalid, clearing storage');
         StorageService.removeData(key: LocalStorageKeys.token);
         StorageService.removeData(key: LocalStorageKeys.user);
+        await _videoCompleter.future;
         Get.offAllNamed(Routes.chooseYoSideScreen);
       }
     } catch (e) {
       StorageService.removeData(key: LocalStorageKeys.token);
       StorageService.removeData(key: LocalStorageKeys.user);
+      await _videoCompleter.future;
       Get.offAllNamed(Routes.chooseYoSideScreen);
     }
   }
