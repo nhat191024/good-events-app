d1366657fe147a23f02b3f1adb4a9b1b32f1c633
Peredar <64675226+perad4r@users.noreply.github.com>
2026-02-08 06:20:36 +0700

    add: blogs showing panel

---
+++ lib/features/client/home/widgets/blogs_panel.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:sukientotapp/features/components/common/blog_card.dart';
+
+class ClientBlogPanel extends StatelessWidget {
+  const ClientBlogPanel({Key? key, required this.blogs}) : super(key: key);
+
+  final List<BlogCard> blogs;
+
+  static const double panelHeight = 300.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: panelHeight,
+      child: ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        scrollDirection: Axis.horizontal,
+        itemCount: blogs.length,
+        separatorBuilder: (context, index) => const SizedBox(width: 12),
+        itemBuilder: (context, index) {
+          return SizedBox(
+            width: MediaQuery.of(context).size.width * 0.7,
+            child: blogs[index],
+          );
+        },
+      ),
+    ).animate(delay: 950.ms).fadeIn(duration: 200.ms);
+  }
+}
