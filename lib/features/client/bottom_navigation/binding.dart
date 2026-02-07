5c5129ba2b838f13d3b101674defdb2584078f4b
Peredar <64675226+perad4r@users.noreply.github.com>
2026-02-08 01:35:49 +0700

    add: bindings for home screen

--- lib/features/client/bottom_navigation/binding.dart
+++ lib/features/client/bottom_navigation/binding.dart
@@ -1,9 +1,12 @@
 import 'package:sukientotapp/core/utils/import/global.dart';
 import 'package:sukientotapp/features/client/bottom_navigation/controller.dart';
+import 'package:sukientotapp/features/client/home/binding.dart';
 
 class ClientBottomNavigationBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<ClientBottomNavigationController>(() => ClientBottomNavigationController());
+
+    HomeBinding().dependencies();
   }
 }
