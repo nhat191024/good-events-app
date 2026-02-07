c8daf1e7d939e57639e3d5a613886838caa0aa70
Peredar <64675226+perad4r@users.noreply.github.com>
2026-02-07 12:18:17 +0700

    update: export the screen

--- lib/core/utils/import/screens.dart
+++ lib/core/utils/import/screens.dart
@@ -3,13 +3,13 @@
 export 'package:sukientotapp/features/common/splash/screen.dart';
 export 'package:sukientotapp/features/common/choose_yo_side/screen.dart';
 export 'package:sukientotapp/features/common/auth/login/screen.dart';
+export 'package:sukientotapp/features/common/introduction/screen.dart';
 
 //Guest
 export 'package:sukientotapp/features/common/home/screen.dart';
 
 //Partner
 export 'package:sukientotapp/features/partner/bottom_navigation/bottom_navigation.dart';
-
 
 //Client
 export 'package:sukientotapp/features/client/bottom_navigation/bottom_navigation.dart';
