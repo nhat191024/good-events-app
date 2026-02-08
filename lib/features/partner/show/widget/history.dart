d1cf217c190db85f4856dab604cffa503b9dfb97
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-02-08 17:07:36 +0700

    feat: add show list for UI Demo

--- lib/features/partner/show/widget/history.dart
+++ lib/features/partner/show/widget/history.dart
@@ -1,10 +1,35 @@
 import 'package:sukientotapp/core/utils/import/global.dart';
 
+import './show.dart';
+
 class HistoryWidget extends StatelessWidget {
   const HistoryWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('histories'.tr, style: context.typography.lg));
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+        child: Column(
+          children: [
+            for (int i = 0; i < 3; i++)
+              Show(
+                code: '$i',
+                timestamp: '10 Giờ',
+                price: '10 VND',
+                clientName: 'Client $i',
+                category: 'Category $i',
+                event: 'Event $i',
+                date: '0${i + 1}-02-2026',
+                startTime: '10:00',
+                endTime: '12:00',
+                address: 'Address $i',
+                note: 'Note $i',
+                currentStatus: 'completed',
+              ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/partner/show/widget/upcoming.dart
+++ lib/features/partner/show/widget/upcoming.dart
@@ -1,10 +1,35 @@
 import 'package:sukientotapp/core/utils/import/global.dart';
 
+import './show.dart';
+
 class UpcomingWidget extends StatelessWidget {
   const UpcomingWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('upcomings'.tr, style: context.typography.lg));
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+        child: Column(
+          children: [
+            for (int i = 0; i < 3; i++)
+              Show(
+                code: '$i',
+                timestamp: '10 Giờ',
+                price: '10 VND',
+                clientName: 'Client $i',
+                category: 'Category $i',
+                event: 'Event $i',
+                date: '0${i + 1}-02-2026',
+                startTime: '10:00',
+                endTime: '12:00',
+                address: 'Address $i',
+                note: 'Note $i',
+                currentStatus: 'confirmed',
+              ),
+          ],
+        ),
+      ),
+    );
   }
 }
