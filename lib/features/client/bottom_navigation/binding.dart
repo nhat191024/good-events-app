c3f555b1f8412b718ca48d845a63bdd7baf73d16
Peredar <64675226+perad4r@users.noreply.github.com>
2026-02-11 21:59:37 +0700

    feat: wire booking bindings

--- lib/features/client/bottom_navigation/binding.dart
+++ lib/features/client/bottom_navigation/binding.dart
@@ -1,6 +1,9 @@
 import 'package:sukientotapp/core/utils/import/global.dart';
 import 'package:sukientotapp/features/client/bottom_navigation/controller.dart';
 import 'package:sukientotapp/features/client/home/binding.dart';
+import 'package:sukientotapp/features/client/partner_detail/binding.dart';
+import 'package:sukientotapp/features/client/booking/binding.dart';
+import 'package:sukientotapp/features/client/order/binding.dart';
 
 class ClientBottomNavigationBinding extends Bindings {
   @override
@@ -8,5 +11,7 @@ class ClientBottomNavigationBinding extends Bindings {
     Get.lazyPut<ClientBottomNavigationController>(() => ClientBottomNavigationController());
 
     HomeBinding().dependencies();
+    ClientBookingBinding().dependencies();
+    ClientOrderBinding().dependencies();
   }
 }
