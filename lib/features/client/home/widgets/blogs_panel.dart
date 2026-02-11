a1be65ba65a990bce578ca0f3203243909b2abd9
Peredar <64675226+perad4r@users.noreply.github.com>
2026-02-11 22:00:35 +0700

    refactor: respect shouldAnimate for blog panel

--- lib/features/client/home/widgets/blogs_panel.dart
+++ lib/features/client/home/widgets/blogs_panel.dart
@@ -1,3 +1,4 @@
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:sukientotapp/features/components/common/blog_card.dart';
@@ -11,20 +12,22 @@ class ClientBlogPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: panelHeight,
-      child: ListView.separated(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        scrollDirection: Axis.horizontal,
-        itemCount: blogs.length,
-        separatorBuilder: (context, index) => const SizedBox(width: 12),
-        itemBuilder: (context, index) {
-          return SizedBox(
-            width: MediaQuery.of(context).size.width * 0.7,
-            child: blogs[index],
-          );
-        },
-      ),
-    ).animate(delay: 950.ms).fadeIn(duration: 200.ms);
+    return Obx(
+      () => SizedBox(
+        height: panelHeight,
+        child: ListView.separated(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          scrollDirection: Axis.horizontal,
+          itemCount: blogs.length,
+          separatorBuilder: (context, index) => const SizedBox(width: 12),
+          itemBuilder: (context, index) {
+            return SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: blogs[index],
+            );
+          },
+        ),
+      ).animate(delay: 500.ms).fadeIn(duration: 200.ms),
+    );
   }
 }
