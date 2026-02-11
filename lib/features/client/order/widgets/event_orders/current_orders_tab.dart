59d3d98b559cd0495f5d03be1f3562b5c4f86dfd
Peredar <64675226+perad4r@users.noreply.github.com>
2026-02-11 22:06:00 +0700

    feat: current event orders tab

---
+++ lib/features/client/order/widgets/event_orders/current_orders_tab.dart
@@ -0,0 +1,77 @@
+import 'package:sukientotapp/core/utils/import/global.dart';
+import '../../controller.dart';
+import 'event_order_card.dart';
+
+class CurrentOrdersTab extends StatelessWidget {
+  const CurrentOrdersTab({super.key, required this.controller});
+
+  final ClientOrderController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      if (controller.isLoadingEventOrders.value) {
+        return _buildLoading();
+      }
+
+      final orders = controller.currentEventOrders;
+
+      if (orders.isEmpty) {
+        return _buildEmpty(context);
+      }
+
+      return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: orders.length,
+        itemBuilder: (context, index) {
+          return EventOrderCard(
+            order: orders[index],
+            isSelected: index == 0, // First item is "viewing"
+          );
+        },
+      );
+    });
+  }
+
+  Widget _buildLoading() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: 3,
+      itemBuilder: (context, index) {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 12),
+          height: 160,
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: const Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildEmpty(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.inbox_outlined,
+            size: 64,
+            color: Colors.grey[300],
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'no_current_orders'.tr,
+            style: context.typography.base.copyWith(
+              color: Colors.grey[500],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
