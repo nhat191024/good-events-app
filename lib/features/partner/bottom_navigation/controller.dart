293a6be51e5dee2025e813ff1b034943294840d6
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-02-25 05:00:32 +0700

    feat: enhance setIndex method to support tab switching in PartnerBottomNavigationController

--- lib/features/partner/bottom_navigation/controller.dart
+++ lib/features/partner/bottom_navigation/controller.dart
@@ -1,11 +1,21 @@
 import 'package:sukientotapp/core/utils/import/global.dart';
+import 'package:sukientotapp/features/partner/show/controller.dart';
 
 class PartnerBottomNavigationController extends GetxController {
   final RxInt currentIndex = 0.obs;
   var isReverse = false.obs;
-  
-  void setIndex(int index) {
+
+  final ShowController showController = Get.find<ShowController>();
+
+  void setIndex(int index, {int? setTab}) {
     isReverse.value = index < currentIndex.value;
     currentIndex.value = index;
+
+    if (index == 1 && setTab != null) {
+      if (Get.isRegistered<ShowController>()) {
+        final showController = Get.find<ShowController>();
+        showController.switchTab(setTab);
+      } else {}
+    }
   }
 }
