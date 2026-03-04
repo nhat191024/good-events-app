22eaed36b8f29dcc5f3c55c6d0597f3e6b316258
Peredar <64675226+perad4r@users.noreply.github.com>
2026-03-04 15:54:32 +0700

    add: location repo implement for getting wards and provinces

---
+++ lib/data/repositories/location_repository_impl.dart
@@ -0,0 +1,34 @@
+import 'package:sukientotapp/data/models/location_model.dart';
+import 'package:sukientotapp/data/providers/location_provider.dart';
+import 'package:sukientotapp/domain/repositories/location_repository.dart';
+import 'package:sukientotapp/core/utils/logger.dart';
+
+class LocationRepositoryImpl implements LocationRepository {
+  final LocationProvider _provider;
+
+  LocationRepositoryImpl(this._provider);
+
+  @override
+  Future<List<LocationModel>> getProvinces() async {
+    try {
+      final rawData = await _provider.getProvinces();
+      return rawData.map((json) => LocationModel.fromJson(json as Map<String, dynamic>)).toList();
+    } catch (e) {
+      logger.e('[LocationRepositoryImpl] [getProvinces] Error: $e');
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<LocationModel>> getWards(int provinceId) async {
+    try {
+      final rawData = await _provider.getWards(provinceId);
+      return rawData.map((json) => LocationModel.fromJson(json as Map<String, dynamic>)).toList();
+    } catch (e) {
+      logger.e(
+        '[LocationRepositoryImpl] [getWards] Error fetching wards for province $provinceId: $e',
+      );
+      rethrow;
+    }
+  }
+}
