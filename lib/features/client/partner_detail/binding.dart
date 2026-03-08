b7e277f57197b99b1b5912659ebf1856b384ef10
Peredar <64675226+perad4r@users.noreply.github.com>
2026-03-08 15:03:43 +0700

    update: inject dependency for home screen

--- lib/features/client/partner_detail/binding.dart
+++ lib/features/client/partner_detail/binding.dart
@@ -1,9 +1,12 @@
 import 'package:get/get.dart';
 import 'controller.dart';
+import 'package:sukientotapp/domain/repositories/client/home_repository.dart';
 
 class PartnerDetailBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<PartnerDetailController>(() => PartnerDetailController());
+    Get.lazyPut<PartnerDetailController>(
+      () => PartnerDetailController(Get.find<HomeRepository>()),
+    );
   }
 }
