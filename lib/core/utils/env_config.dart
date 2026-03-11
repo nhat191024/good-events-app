0e046e8c8a3c502c09582001a00a3efc027f1a57
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-12 01:16:01 +0700

    feat: add pusher configuration getters for environment variables

--- lib/core/utils/env_config.dart
+++ lib/core/utils/env_config.dart
@@ -4,4 +4,12 @@ class EnvConfig {
   static String get apiBaseUrl {
     return dotenv.env['API_BASE_URL'] ?? '';
   }
+
+  static String get pusherAppKey {
+    return dotenv.env['PUSHER_APP_KEY'] ?? '';
+  }
+
+  static String get pusherCluster {
+    return dotenv.env['PUSHER_CLUSTER'] ?? '';
+  }
 }
