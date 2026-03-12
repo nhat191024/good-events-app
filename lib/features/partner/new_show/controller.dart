ceea971adcc7f69b5ea37bc6450263c67f839aa5
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-12 23:46:34 +0700

    feat: implement real-time bill updates with Pusher integration in NewShowController

--- lib/features/partner/new_show/controller.dart
+++ lib/features/partner/new_show/controller.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:pusher_channels_flutter/pusher_channels_flutter.dart';
 import 'package:sukientotapp/core/utils/import/global.dart';
 import 'package:sukientotapp/data/models/partner/partner_bill_model.dart';
 import 'package:sukientotapp/domain/repositories/partner/new_show_repository.dart';
@@ -12,6 +15,10 @@ class NewShowController extends GetxController {
   final bills = <PartnerBill>[].obs;
   final availableCategories = <AvailableCategory>[].obs;
   final lastUpdated = ''.obs;
+
+  final _subscribedChannels = <String>[];
+
+  static const _pusherEventName = 'NewPartnerBillCreated';
 
   @override
   void onInit() {
@@ -37,6 +44,7 @@ class NewShowController extends GetxController {
       availableCategories.assignAll(response.availableCategories);
       lastUpdated.value = response.lastUpdated;
       logger.i('[NewShow] [Fetch] Fetched ${bills.length} bills');
+      await _subscribeToCategories();
     } catch (e) {
       logger.e('[NewShow] [Fetch] Error: $e');
     } finally {
@@ -44,8 +52,55 @@ class NewShowController extends GetxController {
     }
   }
 
+  Future<void> _subscribeToCategories() async {
+    await _unsubscribeAll();
+
+    for (final category in availableCategories) {
+      final channelName = 'private-category.${category.id}';
+      await PusherService.subscribe(
+        channelName: channelName,
+        eventName: _pusherEventName,
+        onEvent: _onPusherEvent,
+      );
+      _subscribedChannels.add(channelName);
+    }
+  }
+
+  void _onPusherEvent(PusherEvent event) {
+    if (event.eventName != _pusherEventName) return;
+    if (event.data == null) return;
+
+    try {
+      final data = jsonDecode(event.data!) as Map<String, dynamic>;
+
+      if (data.containsKey('id') && data.containsKey('code')) {
+        final newBill = PartnerBill.fromMap(data);
+        bills.insert(0, newBill);
+        lastUpdated.value = DateFormat('HH:mm:ss').format(DateTime.now());
+        logger.i('[NewShow] [Pusher] New bill received: ${newBill.code}');
+      } else {
+        final response = RealtimeBillsResponse.fromMap(data);
+        bills.insertAll(0, response.partnerBills);
+        lastUpdated.value = response.lastUpdated;
+        logger.i(
+          '[NewShow] [Pusher] ${response.partnerBills.length} new bill(s) received',
+        );
+      }
+    } catch (e) {
+      logger.e('[NewShow] [Pusher] Error parsing event: $e');
+    }
+  }
+
+  Future<void> _unsubscribeAll() async {
+    for (final channel in _subscribedChannels) {
+      await PusherService.unsubscribe(channel);
+    }
+    _subscribedChannels.clear();
+  }
+
   @override
   void onClose() {
+    _unsubscribeAll();
     scrollController.dispose();
     super.onClose();
   }
