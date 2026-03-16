5c003195aa03588ee029609e9c92dfbd45dbd9f0
Peredar <64675226+perad4r@users.noreply.github.com>
2026-03-16 21:19:39 +0700

    refractor: update imports after controller structure chanegs

--- lib/features/client/order_detail/binding.dart
+++ lib/features/client/order_detail/binding.dart
@@ -1,14 +1,15 @@
 import 'package:sukientotapp/core/utils/import/global.dart';
+import 'package:sukientotapp/core/services/api_service.dart';
 import 'package:sukientotapp/data/providers/client/order_provider.dart';
 import 'package:sukientotapp/data/repositories/client/order_repository_impl.dart';
 import 'package:sukientotapp/domain/repositories/client/order_repository.dart';
-import 'package:dio/dio.dart';
-import 'controller.dart';
+import 'controller/controller.dart';
 
 class ClientOrderDetailBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<OrderProvider>(() => OrderProvider(dio: Get.find<Dio>()));
+    Get.lazyPut<ApiService>(() => ApiService(), fenix: true);
+    Get.lazyPut<OrderProvider>(() => OrderProvider(dio: Get.find<ApiService>().dio));
     Get.lazyPut<OrderRepository>(() => OrderRepositoryImpl(Get.find<OrderProvider>()));
     Get.lazyPut<ClientOrderDetailController>(
       () => ClientOrderDetailController(Get.find<OrderRepository>()),
