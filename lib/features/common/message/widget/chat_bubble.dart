683250fb1374b298f4cab41450d447bbb4f6a9d9
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-17 00:31:30 +0700

    refactor: improve layout and styling in ChatBubble widget

--- lib/features/common/message/widget/chat_bubble.dart
+++ lib/features/common/message/widget/chat_bubble.dart
@@ -22,49 +22,51 @@ class ChatBubble extends StatelessWidget {
             alignment: message.isSender
                 ? Alignment.centerRight
                 : Alignment.centerLeft,
-            child: Column(
-              children: [
-                if (!message.isSender)
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 8,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (!message.isSender)
+                    Text(
                       message.sender,
                       style: const TextStyle(fontSize: 12, color: Colors.grey),
                     ),
-                  ),
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 4,
-                  ),
-                  padding: const EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    color: message.isSender
-                        ? AppColors.primary
-                        : AppColors.lightBackground,
-                    borderRadius: BorderRadius.circular(16).copyWith(
-                      bottomRight: message.isSender
-                          ? const Radius.circular(0)
-                          : null,
-                      bottomLeft: !message.isSender
-                          ? const Radius.circular(0)
-                          : null,
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: message.isSender
+                          ? AppColors.primary
+                          : AppColors.lightBackground,
+                      borderRadius: BorderRadius.circular(16).copyWith(
+                        bottomRight: message.isSender
+                            ? const Radius.circular(0)
+                            : null,
+                        bottomLeft: !message.isSender
+                            ? const Radius.circular(0)
+                            : null,
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          message.text,
+                          style: TextStyle(
+                            color: message.isSender
+                                ? Colors.white
+                                : Colors.black,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        message.text,
-                        style: TextStyle(
-                          color: message.isSender ? Colors.white : Colors.black,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           Padding(
