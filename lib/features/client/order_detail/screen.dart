736e1c2a09df234e26288a55f8a7748297772f00
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-20 22:25:06 +0700

    Remove unused import for pull_to_refresh package

--- lib/features/client/order_detail/screen.dart
+++ lib/features/client/order_detail/screen.dart
@@ -1,4 +1,3 @@
-import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:sukientotapp/core/utils/import/global.dart';
 import 'controller/controller.dart';
 import 'widgets/order_detail_header.dart';
