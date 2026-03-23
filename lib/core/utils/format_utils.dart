242143227993bf49fd08926736ef49b652b8f48c
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-23 17:50:02 +0700

    fix: update currency formatting symbols to use correct Vietnamese terms

--- lib/core/utils/format_utils.dart
+++ lib/core/utils/format_utils.dart
@@ -3,17 +3,17 @@ import 'package:intl/intl.dart';
 class FormatUtils {
   static String formatCurrency(int value) {
     if (value >= 1000000000) {
-      return '${(value / 1000000000).toStringAsFixed(1).replaceAll(RegExp(r'\.0$'), '')}B đ';
+      return '${(value / 1000000000).toStringAsFixed(1).replaceAll(RegExp(r'\.0$'), '')}Tỷ Đ';
     } else if (value >= 1000000) {
-      return '${(value / 1000000).toStringAsFixed(1).replaceAll(RegExp(r'\.0$'), '')}M đ';
+      return '${(value / 1000000).toStringAsFixed(1).replaceAll(RegExp(r'\.0$'), '')}Tr Đ';
     } else if (value >= 1000) {
-      return '${(value / 1000).toStringAsFixed(1).replaceAll(RegExp(r'\.0$'), '')}k đ';
+      return '${(value / 1000).toStringAsFixed(1).replaceAll(RegExp(r'\.0$'), '')}k Đ';
     }
-    return NumberFormat.currency(locale: 'vi_VN', symbol: 'đ').format(value);
+    return NumberFormat.currency(locale: 'vi_VN', symbol: 'Đ').format(value);
   }
 
   /// Format currency with comma every 3 digits and with currency symbol at the end
   static String formatCurrencyToDoule(int value) {
-    return '${NumberFormat('#,##0', 'vi_VN').format(value)} đ';
+    return '${NumberFormat('#,##0', 'vi_VN').format(value)} Đ';
   }
 }
