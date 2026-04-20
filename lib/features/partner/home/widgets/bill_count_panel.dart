3d44854a71ad0f96d87b6c41c94d6786eaa875fb
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-04-21 00:37:14 +0700

    feat: integrate navigation controller in BillCountPanel for item selection

--- lib/features/partner/home/widgets/bill_count_panel.dart
+++ lib/features/partner/home/widgets/bill_count_panel.dart
@@ -1,4 +1,5 @@
 import 'package:sukientotapp/core/utils/import/global.dart';
+import 'package:sukientotapp/features/partner/bottom_navigation/controller.dart';
 
 class BillCountPanel extends StatelessWidget {
   final RxString newShows;
@@ -14,6 +15,7 @@ class BillCountPanel extends StatelessWidget {
   Widget build(BuildContext context) {
     double panelWidth = MediaQuery.of(context).size.width;
     double itemWidth = (panelWidth - 58) / 2;
+    final navController = Get.find<PartnerBottomNavigationController>();
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -24,6 +26,9 @@ class BillCountPanel extends StatelessWidget {
           FIcons.calendarSearch,
           'take_order',
           newShows,
+          () {
+            navController.setIndex(1);
+          },
         ),
         _buildItem(
           context,
@@ -32,71 +37,78 @@ class BillCountPanel extends StatelessWidget {
           FIcons.calendarCheck2,
           'waiting_show',
           waitingConfirmation,
+          () {
+            navController.setIndex(2);
+          },
         ),
       ],
     );
   }
 
-  Container _buildItem(
+  GestureDetector _buildItem(
     BuildContext context,
     double itemWidth,
     Color iconBgColor,
     IconData iconData,
     String title,
     RxString count,
+    void Function()? onTap,
   ) {
-    return Container(
-      width: itemWidth,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.06),
-            blurRadius: 12,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(14, 14, 14, 0),
-            padding: const EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: iconBgColor.withValues(alpha: 0.12),
-              borderRadius: BorderRadius.circular(14),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: itemWidth,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.06),
+              blurRadius: 12,
+              offset: const Offset(0, 4),
             ),
-            child: Icon(iconData, color: iconBgColor, size: 22),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(14, 10, 14, 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title.tr,
-                  style: context.typography.xs.copyWith(
-                    color: context.fTheme.colors.mutedForeground,
-                  ),
-                ),
-                const SizedBox(height: 2),
-                Obx(
-                  () => Text(
-                    count.value,
-                    style: const TextStyle(
-                      color: Color(0xFF1F2937),
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      height: 1.1,
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(14, 14, 14, 0),
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: iconBgColor.withValues(alpha: 0.12),
+                borderRadius: BorderRadius.circular(14),
+              ),
+              child: Icon(iconData, color: iconBgColor, size: 22),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(14, 10, 14, 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title.tr,
+                    style: context.typography.xs.copyWith(
+                      color: context.fTheme.colors.mutedForeground,
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 2),
+                  Obx(
+                    () => Text(
+                      count.value,
+                      style: const TextStyle(
+                        color: Color(0xFF1F2937),
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        height: 1.1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
