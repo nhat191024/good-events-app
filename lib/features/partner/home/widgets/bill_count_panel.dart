6d9ac136e87d098cfbcc08da0fd1f9a530c6f2d3
Peredar <64675226+perad4r@users.noreply.github.com>
2026-04-20 22:13:28 +0700

    update: fixed rage baiting clicks on home screen

--- lib/features/partner/home/widgets/bill_count_panel.dart
+++ lib/features/partner/home/widgets/bill_count_panel.dart
@@ -3,99 +3,140 @@ import 'package:sukientotapp/core/utils/import/global.dart';
 class BillCountPanel extends StatelessWidget {
   final String newShows;
   final String waitingConfirmation;
+  final VoidCallback? onTapNewShows;
+  final VoidCallback? onTapWaitingConfirmation;
 
   const BillCountPanel({
     super.key,
     required this.newShows,
     required this.waitingConfirmation,
+    this.onTapNewShows,
+    this.onTapWaitingConfirmation,
   });
 
   @override
   Widget build(BuildContext context) {
     double panelWidth = MediaQuery.of(context).size.width;
     double itemWidth = (panelWidth - 58) / 2;
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        _buildItem(
-          context,
-          itemWidth,
-          AppColors.primary,
-          FIcons.calendarSearch,
-          'take_order',
-          newShows,
+        _BillItem(
+          width: itemWidth,
+          iconBgColor: AppColors.primary,
+          iconData: FIcons.calendarSearch,
+          title: 'take_order',
+          count: newShows,
+          onTap: onTapNewShows,
         ),
-        _buildItem(
-          context,
-          itemWidth,
-          AppColors.amber500,
-          FIcons.calendarCheck2,
-          'waiting_show',
-          waitingConfirmation,
+        _BillItem(
+          width: itemWidth,
+          iconBgColor: AppColors.amber500,
+          iconData: FIcons.calendarCheck2,
+          title: 'waiting_show',
+          count: waitingConfirmation,
+          onTap: onTapWaitingConfirmation,
         ),
       ],
     );
   }
+}
 
-  Container _buildItem(
-    BuildContext context,
-    double itemWidth,
-    Color iconBgColor,
-    IconData iconData,
-    String title,
-    String count,
-  ) {
-    return Container(
-      width: itemWidth,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.06),
-            blurRadius: 12,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+class _BillItem extends StatefulWidget {
+  final double width;
+  final Color iconBgColor;
+  final IconData iconData;
+  final String title;
+  final String count;
+  final VoidCallback? onTap;
+
+  const _BillItem({
+    required this.width,
+    required this.iconBgColor,
+    required this.iconData,
+    required this.title,
+    required this.count,
+    this.onTap,
+  });
+
+  @override
+  State<_BillItem> createState() => _BillItemState();
+}
+
+class _BillItemState extends State<_BillItem>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _controller?.forward().then((_) => _controller?.reverse());
+        widget.onTap?.call();
+      },
+      child:
           Container(
-            margin: const EdgeInsets.fromLTRB(14, 14, 14, 0),
-            padding: const EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: iconBgColor.withValues(alpha: 0.12),
-              borderRadius: BorderRadius.circular(14),
-            ),
-            child: Icon(iconData, color: iconBgColor, size: 22),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(14, 10, 14, 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title.tr,
-                  style: context.typography.xs.copyWith(
-                    color: context.fTheme.colors.mutedForeground,
-                  ),
+                width: widget.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withValues(alpha: 0.06),
+                      blurRadius: 12,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 2),
-                Text(
-                  count,
-                  style: const TextStyle(
-                    color: Color(0xFF1F2937),
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    height: 1.1,
-                  ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(14, 14, 14, 0),
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: widget.iconBgColor.withValues(alpha: 0.12),
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      child: Icon(
+                        widget.iconData,
+                        color: widget.iconBgColor,
+                        size: 22,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(14, 10, 14, 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.title.tr,
+                            style: context.typography.xs.copyWith(
+                              color: context.fTheme.colors.mutedForeground,
+                            ),
+                          ),
+                          const SizedBox(height: 2),
+                          Text(
+                            widget.count,
+                            style: const TextStyle(
+                              color: Color(0xFF1F2937),
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              height: 1.1,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ],
-      ),
+              )
+              .animate(
+                autoPlay: false,
+                onInit: (controller) => _controller = controller,
+              )
+              .scaleXY(end: 0.95, duration: 100.ms, curve: Curves.easeInOut),
     );
   }
 }
